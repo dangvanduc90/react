26db76167561e3e0fac377c00b494db768bfde8a
dangduc <ducdangvan@vccorp.vn>
2018-06-13 15:47:32 +0700

    chuan bi CRUD cong viec https://www.youtube.com/watch?v=yCW6PP87-OU&index=17&list=PLJ5qtRQovuEOoKffoCBzTfvzMTTORnoyp

--- src/index.js
+++ src/index.js
@@ -297,47 +297,455 @@ import registerServiceWorker from './registerServiceWorker';
 /* ======================================= */
 
 /* ======================================= form */
-class NameForm extends React.Component {
+// class NameForm extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {isGoing: true, numberOfGuest: 2};
+//         this.handleChange = this.handleChange.bind(this);
+//     }
+//
+//     handleChange(event) {
+//         let value;
+//         let name = event.target.name;
+//         if (event.target.type === "checkbox") {
+//             value = event.target.checked
+//         } else {
+//             value = event.target.value;
+//         }
+//         this.setState({
+//             [name]: value
+//         });
+//     }
+//
+//     render() {
+//         return (
+//             <form action="#">
+//                 <label htmlFor="">Is Going:
+//                     <input type="checkbox" name="isGoing" checked={this.state.isGoing} onChange={this.handleChange} />
+//                 </label>
+//                 <br/>
+//                 <label htmlFor="">Is Going:
+//                     <input type="number" name="numberOfGuest" checked={this.state.numberOfGuest} onChange={this.handleChange} />
+//                 </label>
+//                 <input type="submit" value="Submit" />
+//             </form>
+//         )
+//     }
+// }
+// ReactDOM.render(
+//     <NameForm />,
+//     document.getElementById('root')
+// );
+/* ======================================= */
+
+/* ======================================= Containment */
+// function FancyBorder(props) {
+//     return (
+//         <div className={'FancyBorder FancyBorder-' + props.color}>
+//             {props.children}
+//         </div>
+//     );
+// }
+//
+// function WelcomeDialog() {
+//     return (
+//         <FancyBorder color="blue">
+//             <h1 className="Dialog-title">
+//                 Welcome
+//             </h1>
+//             <p className="Dialog-message">
+//                 Thank you for visiting our spacecraft!
+//             </p>
+//         </FancyBorder>
+//     );
+// }
+// ReactDOM.render(
+//     <WelcomeDialog />,
+//     document.getElementById('root')
+// );
+/* ======================================= */
+
+/* ======================================= Containment */
+// function SplitPane(props) {
+//     return (
+//         <div className="SplitPane">
+//             <div className="SplitPaneLeft">
+//                 {props.left}
+//             </div>
+//             <div className="SplitPaneRight">
+//                 {props.right}
+//             </div>
+//         </div>
+//     );
+// }
+// function Contacts() {
+//     return <div className="Contacts">Contacts</div>;
+// }
+// function Chat() {
+//     return <div className="Chat">Chat</div>;
+// }
+// function App() {
+//     return (
+//         <SplitPane left={<Contacts/>} right={<Chat/>} />
+//     );
+// }
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// );
+/* ======================================= */
+
+/* ======================================= Specialization */
+// function FancyBorder(props) {
+//     return (
+//         <div className={'FancyBorder FancyBorder-blue'}>
+//             <h1 className="Dialog-title">
+//                 {props.title}
+//             </h1>
+//             <p className="Dialog-message">
+//                 {props.desc}
+//             </p>
+//             {props.children}
+//         </div>
+//     );
+// }
+// class SingupDialog extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {login: ""};
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSingUp = this.handleSingUp.bind(this);
+//     }
+//     handleChange(e) {
+//         this.setState({login: e.target.value});
+//     }
+//     handleSingUp() {
+//         alert("Welcome " + this.state.login + "!");
+//     }
+//     render() {
+//         return (
+//             <FancyBorder
+//                 title="Praesent"
+//                 desc="Praesent sapien massa, convallis a pellentesque nec, egestas non nisi." >
+//                 <input type="text" value={this.state.login} onChange={this.handleChange} />
+//                 <button onClick={this.handleSingUp}>Sing me up</button>
+//             </FancyBorder>
+//         )
+//     }
+// }
+// ReactDOM.render(
+//     <SingupDialog />,
+//     document.getElementById('root')
+// );
+/* ======================================= */
+
+
+/* ======================================= Thinking in React */
+// const products = [
+//     {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+//     {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+//     {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+//     {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+//     {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+//     {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+// ];
+// function ProductRow(props) {
+//     return (
+//         <tr>
+//             <td style={props.product.stocked ? {color: "red"} : {}}>{props.product.name}</td>
+//             <td>{props.product.price}</td>
+//         </tr>
+//     )
+// }
+// function ProductCategoryRow(props) {
+//     const categories = props.categories;
+//     return (
+//         <tr align="middle">
+//             <td className="productPrice" colSpan="2"><b>{categories}</b></td>
+//         </tr>
+//     )
+// }
+// class ProductTable extends React.Component {
+//     render() {
+//         const filterText = this.props.filterText;
+//         const inStockOnly = this.props.inStockOnly;
+//         let rows = [];
+//         let last_category = null;
+//         let products = this.props.products;
+//         products.forEach((product) => {
+//             if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+//                 return;
+//             }
+//             if (inStockOnly) {
+//                 if (!product.stocked) {
+//                     return;
+//                 }
+//             }
+//             if (product.category !== last_category) {
+//                 rows.push(<ProductCategoryRow key={product.category} categories={product.category} />)
+//             }
+//             last_category = product.category;
+//             rows.push(<ProductRow key={product.name} product={product} />);
+//         });
+//         return (
+//             <table>
+//                 <thead>
+//                     <tr>
+//                         <th><b>Name</b></th>
+//                         <th><b>Price</b></th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     {rows}
+//                 </tbody>
+//             </table>
+//         )
+//     }
+// }
+// class SearchBar extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.handleFilterTextChangeSearch = this.handleFilterTextChangeSearch.bind(this);
+//         this.handleInStockChangeSearch = this.handleInStockChangeSearch.bind(this);
+//     }
+//
+//     handleFilterTextChangeSearch(e) {
+//         this.props.onFilterTextChange(e.target.value);
+//     }
+//
+//     handleInStockChangeSearch(e) {
+//         this.props.onInStockChange(e.target.checked);
+//     }
+//
+//     render() {
+//         return (
+//             <form action="">
+//                 <input type="text" name="keyword" placeholder="Search.." onChange={this.handleFilterTextChangeSearch} value={this.props.filterText} />
+//                 <p>
+//                     <input type="checkbox" name="only_stock" onChange={this.handleInStockChangeSearch} checked={this.props.inStockOnly} />
+//                     Only show products in stock
+//                 </p>
+//             </form>
+//         )
+//     }
+// }
+// class ProductTableWrapper extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             filterText: '',
+//             inStockOnly: false
+//         };
+//         this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+//         this.handleInStockChange = this.handleInStockChange.bind(this);
+//     }
+//
+//     handleFilterTextChange(filterText) {
+//         this.setState({filterText: filterText});
+//     }
+//
+//     handleInStockChange(inStockOnly) {
+//         this.setState({inStockOnly: inStockOnly});
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <SearchBar
+//                     filterText={this.state.filterText}
+//                     inStockOnly={this.state.inStockOnly}
+//                     onFilterTextChange={this.handleFilterTextChange}
+//                     onInStockChange={this.handleInStockChange}
+//                 />
+//                 <ProductTable
+//                     products={products}
+//                     filterText={this.state.filterText}
+//                     inStockOnly={this.state.inStockOnly}
+//                 />
+//             </div>
+//         )
+//     }
+// }
+// ReactDOM.render(
+//     <ProductTableWrapper />,
+//     document.getElementById('root')
+// );
+/* ======================================= */
+
+/* ======================================= Ref */
+// class RefTest extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.handleOnChange = this.handleOnChange.bind(this);
+//     }
+//
+//     handleOnChange(e) {
+//         console.log(this.refs.username.value);
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <input type="text" ref="username" onChange={this.handleOnChange} />
+//                 <input type="text" ref="password" onChange={this.handleOnChange} />
+//             </div>
+//         );
+//     }
+// }
+// ReactDOM.render(
+//     <RefTest />,
+//     document.getElementById('root')
+// );
+/* ======================================= */
+
+/* ======================================= form */
+class App extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {isGoing: true, numberOfGuest: 2};
-        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            'txtUsername': 'dangduc90',
+            'txtPassword': '123456',
+            'slGender': 1,
+            'rdLanguage': 'vi',
+            'football': true,
+            'golf': true,
+        };
+        this.handeSubmit = this.handeSubmit.bind(this);
+        this.onHandleChange = this.onHandleChange.bind(this);
     }
 
-    handleChange(event) {
-        let value;
-        let name = event.target.name;
-        if (event.target.type === "checkbox") {
-            value = event.target.checked
-        } else {
-            value = event.target.value;
-        }
+    onHandleChange(e) {
+        let target = e.target;
+        let name = target.name;
+        let value = target.type === 'checkbox' ? target.checked : target.value;
+        // console.log(value);
         this.setState({
             [name]: value
-        });
-        console.log([name], value);
+        })
     }
 
-    render() {
+    handeSubmit(e) {
+        e.preventDefault();
+        console.log(this.state);
+    }
+    render () {
         return (
-            <form action="#">
-                <label htmlFor="">Is Going:
-                    <input type="checkbox" name="isGoing" checked={this.state.isGoing} onChange={this.handleChange} />
-                </label>
-                <br/>
-                <label htmlFor="">Is Going:
-                    <input type="number" name="numberOfGuest" checked={this.state.numberOfGuest} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
+            <form onSubmit={this.handeSubmit}>
+                <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="txtUsername"
+                        aria-describedby="usernameHelp"
+                        placeholder="Enter username"
+                        value={this.state.txtUsername}
+                        onChange={this.onHandleChange}/>
+                    <small id="usernameHelp" className="form-text text-muted">We'll never share your username with anyone
+                        else.
+                    </small>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        name="txtPassword"
+                        value={this.state.txtPassword}
+                        placeholder="Password"
+                        onChange={this.onHandleChange}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="gender">Gender</label>
+                    <select
+                        className="form-control"
+                        id="gender"
+                        name="slGender"
+                        value={this.state.slGender}
+                        onChange={this.onHandleChange}>
+                        <option value={1}>Male</option>
+                        <option value={0}>Femail</option>
+                    </select>
+                </div>
+                <div className="rd-language">
+                    <p>Language</p>
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            name="rdLanguage"
+                            type="radio"
+                            value="vi"
+                            checked={this.state.rdLanguage === 'vi'}
+                            onChange={this.onHandleChange} />
+                        <label className="form-check-label" htmlFor="vi">
+                            vi
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            name="rdLanguage"
+                            type="radio"
+                            value="en"
+                            checked={this.state.rdLanguage === 'en'}
+                            onChange={this.onHandleChange} />
+                        <label className="form-check-label" htmlFor="en">
+                            en
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            name="rdLanguage"
+                            type="radio"
+                            value="fr"
+                            checked={this.state.rdLanguage === 'fr'}
+                            onChange={this.onHandleChange} />
+                        <label className="form-check-label" htmlFor="fr">
+                            fr
+                        </label>
+                    </div>
+                </div>
+                <div className="ck-hobby">
+                    <p>Hobby</p>
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            name="golf"
+                            id="golf"
+                            value="Golf"
+                            checked={this.state.golf === true}
+                            onChange={this.onHandleChange} />
+                        <label className="form-check-label" htmlFor="golf">
+                            Golf
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            name="football"
+                            id="football"
+                            value="Football"
+                            checked={this.state.football === true}
+                            onChange={this.onHandleChange} />
+                        <label className="form-check-label" htmlFor="football">
+                            Football
+                        </label>
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         )
     }
 }
-ReactDOM.render(
-    <NameForm />,
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// );
 /* ======================================= */
+
+
 
 
 

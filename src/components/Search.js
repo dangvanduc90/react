5ab10c09b1d42692e7c1011ddb9f81522aee0d75
dangduc <ducdangvan@vccorp.vn>
2018-06-18 11:15:42 +0700

    Hoan thanh chuc nang Tim kiem va Filter

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,42 @@
 import React, {Component} from "react";
 
 class Search extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword: '',
+        }
+    }
+
+    onChange = (e) => {
+        let target = e.target;
+        let name = target.name;
+        let value = target.value;
+
+        this.setState({
+            [name]: value,
+        });
+    };
+
+    onSearch = () => {
+        this.props.onSearch(this.state.keyword);
+    };
+
     render() {
+        let {keyword} = this.state;
         return (
             <div className="input-group">
-                <input type="text" name="keyword" defaultValue className="form-control" placeholder="Nhập từ khóa..."/>
+                <input
+                    type="text"
+                    name="keyword"
+                    className="form-control"
+                    placeholder="Nhập từ khóa..."
+                    value={keyword}
+                    onChange={this.onChange}
+                />
                 <span className="input-group-btn">
-                    <button className="btn btn-primary" type="button">
+                    <button className="btn btn-primary" type="button" onClick={this.onSearch}>
                         <span className="fa fa-search mr-5">Tìm</span>
                     </button>
                 </span>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,8 +2,30 @@ import React, {Component} from "react";
 import TaskItem from "./TaskItem";
 
 class TaskList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterName: "",
+            filterStatus: -1 // -1 all, 0 deactive, 1 active
+        }
+    }
+
+    onChange = (e) => {
+        let target = e.target;
+        let name = target.name;
+        let value = target.value;
+        this.setState({
+            [name]: value
+        });
+        this.props.onFilter(
+            name === "filterName" ? value : this.state.filterName,
+            name === "filterStatus" ? value : this.state.filterStatus,
+        );
+    };
+
     render() {
-        let tasks = this.props.tasks;
+        let {tasks, filterName, filterStatus} = this.props;
         let element = tasks.map((task, index) =>
             {
                 return <TaskItem
@@ -30,12 +52,25 @@ class TaskList extends Component {
                     <tr>
                         <td>
                         </td>
-                        <td><input type="text" className="form-control" name="filterName"/></td>
                         <td>
-                            <select className="form-control" name="filterStatus">
-                                <option value="{-1}">Tất Cả</option>
-                                <option value="{0}">Ẩn</option>
-                                <option value="{1}">Kích Hoạt</option>
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="filterName"
+                                value={filterName}
+                                onChange={this.onChange}
+                            />
+                        </td>
+                        <td>
+                            <select
+                                className="form-control"
+                                name="filterStatus"
+                                value={filterStatus}
+                                onChange={this.onChange}
+                            >
+                                <option value={-1}>Tất Cả</option>
+                                <option value={0}>Ẩn</option>
+                                <option value={1}>Kích Hoạt</option>
                             </select>
                         </td>
                         <td>
